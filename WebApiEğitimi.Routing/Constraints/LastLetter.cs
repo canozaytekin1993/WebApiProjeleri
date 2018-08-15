6ecf0058b5b63326dcae5d604bf39b3548d638f4
Can Özaytekin <canozaytekin@hotmail.com>
2018-08-15 20:18:56 +0300

    a

---
+++ WebApiEğitimi.Routing/Constraints/LastLetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http.Routing;
+
+namespace WebApiEğitimi.Routing.Constraints
+{
+    public class LastLetter : IHttpRouteConstraint
+    {
+        public bool Match(HttpRequestMessage request, IHttpRoute route, string parameterName, IDictionary<string, object> values,
+            HttpRouteDirection routeDirection)
+        {
+            string paramVal = values[parameterName].ToString();
+
+            if (paramVal.EndsWith("a") || paramVal.EndsWith("b") || paramVal.EndsWith("c"))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ WebApiEğitimi.Routing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiEğitimi.Routing.MvcApplication" Language="C#" %>
